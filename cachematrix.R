b7c7f105808666ca48c4d0e17d8d299589177680
Kevin <kevincni@gmail.com>
2016-02-07 13:50:07 -0700

    #Edited Week 3 Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# Make object to store a mtrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+#set matrix to null
+    inv <- NULL
+#set x to y; reset inv to null
+    set <- function(y) {x <<- y; inv <<- NULL}
+#return vector x
+    get <- function() x
+#set inv to inverse
+    setInverse <- function(inverse) inv <<- inverse
+#return the inverse inv
+    getInverse <- function() inv
+#return special vector    
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse) 
 }
 
 
-## Write a short comment describing this function
+# Return the inverse matrix of input
+# Retrieves cached inverse if input
+# is same as last previous. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
+
+#test caching capability
+mat1<-matrix(c(seq(1:4),seq(6:10)),nrow=3,ncol=3)
+CachedMatrix<-makeCacheMatrix(mat1)
+cacheSolve(CachedMatrix)
+cacheSolve(CachedMatrix)
